063a10621b6bfe9d07cc40056ae432af0bc8dacc
khadijah2112 <khadijahabdulrazaq21@gmail.com>
2020-03-26 16:09:10 -0700

    'final

---
+++ Task 3/script.js
@@ -0,0 +1,20 @@
+var myName = "Abdulrazaq Khadijah";
+
+var myCourses = ['Html', 'css', 'javascript', 'nodejs']
+
+var myCourses = ['Html', 'css', 'javascript', 'nodejs' ]
+
+document.getElementById("name").innerHTML ='<h1>' + myName; + '</h1>';
+document.getElementById("courses").innerHTML = " " + myCourses;
+console.log(myName);
+console.log(myCourses);
+
+if ((myCourses.length % 2)  == 0 ) {
+	var number = [];
+	number.length = 201;
+	for (var i = 0; i < number.length; i++) {
+		if ((i % 2) == 0) {
+			console.log(i);
+		}
+	}
+}
