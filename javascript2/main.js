7d7815213d24ecf7a918015ecf006ce23d857689
khadijah2112 <khadijahabdulrazaq21@gmail.com>
2020-04-09 20:29:13 -0700

    java2

---
+++ javascript2/main.js
@@ -0,0 +1,15 @@
+let name = "Abdulrazaq Khadijah"
+let courses = ['HTML', 'CSS', 'JAVASCRIPT', 'UI/UX DESIGN']
+
+
+console.log(name);
+console.log(courses);
+
+
+var i;
+for (i = 1; i <= 200; i++) {
+    if (i % 2 === 0)
+
+        console.log(i + '');
+
+}
---
+++ javascript2/script.js
@@ -0,0 +1,32 @@
+const data = [
+    { principal: 2500, time: 1.8 },
+    { principal: 1000, time: 5 },
+    { principal: 3000, time: 1 },
+    { principal: 2000, time: 3 },
+];
+
+let rate = 0;
+let interest = 0;
+let interestData = [];
+
+function interestCalculator(arr) {
+    arr.forEach((element) => {
+        if (element.principal < 2500 || element.time <= 1)
+            rate = 2;
+        else if (element.principal >= 2500 && element.time >= 3) rate = 4;
+        else if (element.principal >= 2500 && (element.time > 1 && element.time < 3))
+            rate = 3;
+        else
+            rate = 1;
+        interest = (element.principal * rate * element.time) / 100;
+        interestData.push({
+            principal: element.principal,
+            rate,
+            time: element.time,
+            interest
+        });
+    });
+    console.log(interestData);
+    return interestData;
+};
+interestCalculator(data);
